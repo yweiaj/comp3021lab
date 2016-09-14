998e2ee978875b7708be91e7789144448e68ad27
WEI Yue <yweiaj@ust.hk>
2016-09-14 11:21:04 +0800

    final lab 2

--- src/base/ImageNote.java
+++ src/base/ImageNote.java
@@ -1,10 +1,9 @@
 package base;
 
-//import java.io.File;
 
 public class ImageNote extends Note {
 	
-	//public File image= new File();
+	public java.io.File image;
 
 	public ImageNote(String title) {
 		super(title);
